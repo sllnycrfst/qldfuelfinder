9f56c784169d7bc9ea3b656a73595e4d9c6f74a5
Mitchell <hello@sellanycarfast.com.au>
2025-06-06 15:13:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Map setup
-  const initialZoom = isMobile() ? 12 : 12;
+  const initialZoom = isMobile() ? 18 : 18;
   const initialCenter = [-27.4698, 153.0251];
   const map = L.map("map").setView(initialCenter, initialZoom);
 
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
       attribution:
         '<a href="https://jawg.io" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
-      maxZoom: 16,
+      maxZoom: 18,
     }
   ).addTo(map);
 
