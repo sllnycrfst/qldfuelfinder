992ff74880e188bf37600fb548739d986c2fea34
Mitchell <hello@sellanycarfast.com.au>
2025-06-06 11:26:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
   async function fetchData() {
     try {
       const [siteRes, priceRes] = await Promise.all([
-        fetch("/data/sites.json").then(r => r.json()),
+        fetch("data/sites.json").then(r => r.json()),
         fetch("https://fuel-proxy-1l9d.onrender.com/prices").then(r => r.json())
       ]);
 
