8d0eab23fffaaa804e4e6f6966f018fcfd4680f2
Mitchell <hello@sellanycarfast.com.au>
2025-06-09 15:40:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     return /Mobi|Android/i.test(navigator.userAgent);
   }
 
-  const defaultZoom = isMobile() ? 12 : 12;
+  const defaultZoom = isMobile() ? 16 : 18;
   const defaultCenter = [-27.4698, 153.0251];
 
   // Ask for geolocation BEFORE map is created
