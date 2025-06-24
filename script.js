e5110da676fa2c80fee689bfa9b63fc5451ecfd8
Mitchell <hello@sellanycarfast.com.au>
2025-06-24 16:10:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function showWeather(lat, lon) {
     try {
-      const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`);
+      const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&timezone=auto`);
       const data = await res.json();
       const weather = data.current_weather;
       if (!weather) return;
