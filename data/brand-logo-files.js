f2f8bf0f466604a75f80bf9017d4439c640de58d
Mitchell <hello@sellanycarfast.com.au>
2025-07-20 08:51:21 +1000

    Add files via upload

--- data/brand-logo-files.js
+++ data/brand-logo-files.js
@@ -1,23 +1,33 @@
 // Brand logo file mappings for actual PNG files in /images/ directory
 export const BRAND_LOGO_FILES = {
-  // Caltex/Ampol
-  2: 'images/110.png',
-  3421066: 'images/110.png', // Ampol
-  
   // BP
   5: 'images/2.png',
-  
-  // Shell
-  20: 'images/5094.png',
-  
-  // 7-Eleven
-  113: 'images/57.png',
   
   // Puma
   5094: 'images/5.png',
   
+  // Costco
+  2031031: 'images/12.png',
+  
+  // Speedway
+  167: 'images/16.png',
+  
+  // Coles/Woolworths
+  111: 'images/20.png',
+  
+  // Additional brands
+  86: 'images/23.png',   // Liberty
+  65: 'images/26.png',   // Petrogas
+  
+  // 7-Eleven
+  113: 'images/57.png',
+  
   // Metro
   57: 'images/86.png',
+  
+  // Caltex/Ampol
+  2: 'images/110.png',
+  3421066: 'images/110.png', // Ampol
   
   // United
   23: 'images/111.png',
@@ -32,18 +42,21 @@ export const BRAND_LOGO_FILES = {
   // Pearl
   3421139: 'images/169.png',
   
-  // Costco
-  2031031: 'images/12.png',
+  // Pacific (additional)
+  2418995: 'images/2418994.png', // Vibe
   
-  // Speedway
-  167: 'images/16.png',
+  // Enhance
+  2419037: 'images/2419037.png',
   
-  // Woolworths/Coles
-  111: 'images/20.png', // Coles
+  // Additional PNG files based on your BRAND_NAMES
+  3421028: 'images/3421028.png', // X Convenience
+  3421073: 'images/3421073.png', // EG Ampol
+  3421183: 'images/3421183.png', // U-Go
+  3421193: 'images/3421193.png', // Reddy Express (Shell Reddy)
+  3421202: 'images/3421202.png', // Atlas
   
-  // Additional brands based on your files
-  86: 'images/23.png',   // Liberty
-  65: 'images/26.png',   // Petrogas
+  // Shell (main)
+  20: 'images/5094.png',
   
   // Default fallback
   0: 'images/default.png'
