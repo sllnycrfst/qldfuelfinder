ada033ec2d507f79f7737594ad3a87cd06718178
Mitchell <hello@sellanycarfast.com.au>
2025-08-03 08:08:04 +1000

    Add files via upload

---
+++ data/qld_suburbs.js
@@ -0,0 +1,264 @@
+// QLD Suburbs Data - Browser Compatible Format
+window.QLD_SUBURBS = [
+  // Brisbane Region
+  { suburb: "Brisbane City", postcode: "4000", lat: -27.4698, lng: 153.0251 },
+  { suburb: "Spring Hill", postcode: "4000", lat: -27.4609, lng: 153.0260 },
+  { suburb: "Petrie Terrace", postcode: "4000", lat: -27.4625, lng: 153.0161 },
+  { suburb: "Fortitude Valley", postcode: "4006", lat: -27.4563, lng: 153.0333 },
+  { suburb: "New Farm", postcode: "4005", lat: -27.4679, lng: 153.0464 },
+  { suburb: "Kangaroo Point", postcode: "4169", lat: -27.4804, lng: 153.0351 },
+  { suburb: "South Brisbane", postcode: "4101", lat: -27.4762, lng: 153.0178 },
+  { suburb: "West End", postcode: "4101", lat: -27.4861, lng: 153.0033 },
+  { suburb: "Woolloongabba", postcode: "4102", lat: -27.4974, lng: 153.0351 },
+  { suburb: "Milton", postcode: "4064", lat: -27.4691, lng: 152.9989 },
+  { suburb: "Toowong", postcode: "4066", lat: -27.4850, lng: 152.9926 },
+  { suburb: "St Lucia", postcode: "4067", lat: -27.4975, lng: 152.9934 },
+  { suburb: "Indooroopilly", postcode: "4068", lat: -27.4992, lng: 152.9737 },
+  { suburb: "Paddington", postcode: "4064", lat: -27.4600, lng: 152.9997 },
+  { suburb: "Red Hill", postcode: "4059", lat: -27.4515, lng: 153.0040 },
+  { suburb: "Kelvin Grove", postcode: "4059", lat: -27.4488, lng: 153.0125 },
+  { suburb: "Herston", postcode: "4006", lat: -27.4474, lng: 153.0262 },
+  { suburb: "Bowen Hills", postcode: "4006", lat: -27.4443, lng: 153.0395 },
+  { suburb: "Newstead", postcode: "4006", lat: -27.4474, lng: 153.0462 },
+  { suburb: "Teneriffe", postcode: "4005", lat: -27.4548, lng: 153.0486 },
+  { suburb: "Ascot", postcode: "4007", lat: -27.4286, lng: 153.0629 },
+  { suburb: "Hamilton", postcode: "4007", lat: -27.4379, lng: 153.0642 },
+  { suburb: "Clayfield", postcode: "4011", lat: -27.4172, lng: 153.0521 },
+  { suburb: "Albion", postcode: "4010", lat: -27.4299, lng: 153.0442 },
+  { suburb: "Windsor", postcode: "4030", lat: -27.4361, lng: 153.0316 },
+  { suburb: "Lutwyche", postcode: "4030", lat: -27.4209, lng: 153.0329 },
+  { suburb: "Kedron", postcode: "4031", lat: -27.4077, lng: 153.0296 },
+  { suburb: "Chermside", postcode: "4032", lat: -27.3858, lng: 153.0343 },
+  { suburb: "Stafford", postcode: "4053", lat: -27.4111, lng: 153.0124 },
+  { suburb: "Everton Park", postcode: "4053", lat: -27.4039, lng: 152.9910 },
+  { suburb: "Mitchelton", postcode: "4053", lat: -27.4169, lng: 152.9788 },
+  { suburb: "Ashgrove", postcode: "4060", lat: -27.4436, lng: 152.9872 },
+  { suburb: "The Gap", postcode: "4061", lat: -27.4460, lng: 152.9519 },
+  { suburb: "Coorparoo", postcode: "4151", lat: -27.4957, lng: 153.0574 },
+  { suburb: "Camp Hill", postcode: "4152", lat: -27.4938, lng: 153.0775 },
+  { suburb: "Morningside", postcode: "4170", lat: -27.4680, lng: 153.0719 },
+  { suburb: "Bulimba", postcode: "4171", lat: -27.4516, lng: 153.0629 },
+  { suburb: "Hawthorne", postcode: "4171", lat: -27.4599, lng: 153.0584 },
+  { suburb: "Carindale", postcode: "4152", lat: -27.5047, lng: 153.1015 },
+  { suburb: "Mount Gravatt", postcode: "4122", lat: -27.5388, lng: 153.0809 },
+  { suburb: "Holland Park", postcode: "4121", lat: -27.5222, lng: 153.0642 },
+  { suburb: "Tarragindi", postcode: "4121", lat: -27.5334, lng: 153.0463 },
+  { suburb: "Annerley", postcode: "4103", lat: -27.5103, lng: 153.0333 },
+  { suburb: "Yeronga", postcode: "4104", lat: -27.5197, lng: 153.0138 },
+  { suburb: "Fairfield", postcode: "4103", lat: -27.5087, lng: 153.0260 },
+  { suburb: "Dutton Park", postcode: "4102", lat: -27.4957, lng: 153.0316 },
+  { suburb: "Moorooka", postcode: "4105", lat: -27.5346, lng: 153.0244 },
+  { suburb: "Rocklea", postcode: "4106", lat: -27.5432, lng: 153.0022 },
+  { suburb: "Salisbury", postcode: "4107", lat: -27.5460, lng: 153.0343 },
+  { suburb: "Nathan", postcode: "4111", lat: -27.5574, lng: 153.0627 },
+  { suburb: "Coopers Plains", postcode: "4108", lat: -27.5658, lng: 153.0407 },
+  { suburb: "Sunnybank", postcode: "4109", lat: -27.5793, lng: 153.0551 },
+  { suburb: "Eight Mile Plains", postcode: "4113", lat: -27.5774, lng: 153.0876 },
+  { suburb: "Runcorn", postcode: "4113", lat: -27.5908, lng: 153.0820 },
+  { suburb: "Kuraby", postcode: "4112", lat: -27.6070, lng: 153.0908 },
+  { suburb: "Underwood", postcode: "4119", lat: -27.6012, lng: 153.1132 },
+  { suburb: "Rochedale", postcode: "4123", lat: -27.5774, lng: 153.1311 },
+  { suburb: "Mansfield", postcode: "4122", lat: -27.5388, lng: 153.0937 },
+  { suburb: "Wishart", postcode: "4122", lat: -27.5547, lng: 153.0963 },
+  { suburb: "Upper Mount Gravatt", postcode: "4122", lat: -27.5589, lng: 153.0809 },
+  { suburb: "Garden City", postcode: "4122", lat: -27.5624, lng: 153.0809 },
+  { suburb: "Macgregor", postcode: "4109", lat: -27.5680, lng: 153.0711 },
+  { suburb: "Robertson", postcode: "4109", lat: -27.5647, lng: 153.0604 },
+  { suburb: "Wynnum", postcode: "4178", lat: -27.4426, lng: 153.1732 },
+  { suburb: "Manly", postcode: "4179", lat: -27.4548, lng: 153.1886 },
+  { suburb: "Lota", postcode: "4179", lat: -27.4679, lng: 153.1819 },
+  { suburb: "Tingalpa", postcode: "4173", lat: -27.4746, lng: 153.1259 },
+  { suburb: "Cannon Hill", postcode: "4170", lat: -27.4713, lng: 153.0963 },
+  { suburb: "Norman Park", postcode: "4170", lat: -27.4810, lng: 153.0653 },
+  { suburb: "Greenslopes", postcode: "4120", lat: -27.5103, lng: 153.0510 },
+  { suburb: "Stones Corner", postcode: "4120", lat: -27.4985, lng: 153.0434 },
+  { suburb: "Taringa", postcode: "4068", lat: -27.4925, lng: 152.9801 },
+  { suburb: "Auchenflower", postcode: "4066", lat: -27.4746, lng: 152.9917 },
+  { suburb: "Bardon", postcode: "4065", lat: -27.4599, lng: 152.9758 },
+  { suburb: "Rainworth", postcode: "4065", lat: -27.4628, lng: 152.9681 },
+  { suburb: "Enoggera", postcode: "4051", lat: -27.4263, lng: 152.9899 },
+  { suburb: "Alderley", postcode: "4051", lat: -27.4286, lng: 152.9997 },
+  { suburb: "Newmarket", postcode: "4051", lat: -27.4361, lng: 153.0040 },
+  { suburb: "Wilston", postcode: "4051", lat: -27.4336, lng: 153.0168 },
+  { suburb: "Grange", postcode: "4051", lat: -27.4238, lng: 153.0168 },
+  { suburb: "Gordon Park", postcode: "4031", lat: -27.4019, lng: 153.0296 },
+  { suburb: "Nundah", postcode: "4012", lat: -27.4019, lng: 153.0719 },
+  { suburb: "Northgate", postcode: "4013", lat: -27.3900, lng: 153.0673 },
+  { suburb: "Virginia", postcode: "4014", lat: -27.3731, lng: 153.0627 },
+  { suburb: "Wavell Heights", postcode: "4012", lat: -27.3943, lng: 153.0476 },
+  { suburb: "Banyo", postcode: "4014", lat: -27.3743, lng: 153.0784 },
+  { suburb: "Nudgee", postcode: "4014", lat: -27.3645, lng: 153.0872 },
+  { suburb: "Hendra", postcode: "4011", lat: -27.4240, lng: 153.0695 },
+  { suburb: "Eagle Farm", postcode: "4009", lat: -27.4336, lng: 153.0784 },
+  { suburb: "Pinkenba", postcode: "4008", lat: -27.4121, lng: 153.1162 },
+  { suburb: "Wynnum West", postcode: "4178", lat: -27.4590, lng: 153.1546 },
+  { suburb: "Hemmant", postcode: "4174", lat: -27.4467, lng: 153.1299 },
+  { suburb: "Murarrie", postcode: "4172", lat: -27.4548, lng: 153.0989 },
+  { suburb: "Moreton Island", postcode: "4025", lat: -27.1634, lng: 153.3977 },
+  
+  // Gold Coast
+  { suburb: "Southport", postcode: "4215", lat: -27.9674, lng: 153.4143 },
+  { suburb: "Main Beach", postcode: "4217", lat: -27.9674, lng: 153.4284 },
+  { suburb: "Surfers Paradise", postcode: "4217", lat: -28.0023, lng: 153.4310 },
+  { suburb: "Broadbeach", postcode: "4218", lat: -28.0336, lng: 153.4290 },
+  { suburb: "Mermaid Beach", postcode: "4218", lat: -28.0511, lng: 153.4373 },
+  { suburb: "Miami", postcode: "4220", lat: -28.0721, lng: 153.4412 },
+  { suburb: "Burleigh Heads", postcode: "4220", lat: -28.0896, lng: 153.4451 },
+  { suburb: "Palm Beach", postcode: "4221", lat: -28.1177, lng: 153.4673 },
+  { suburb: "Currumbin", postcode: "4223", lat: -28.1466, lng: 153.4841 },
+  { suburb: "Coolangatta", postcode: "4225", lat: -28.1685, lng: 153.5377 },
+  { suburb: "Robina", postcode: "4226", lat: -28.0721, lng: 153.3933 },
+  { suburb: "Nerang", postcode: "4211", lat: -28.0023, lng: 153.3387 },
+  { suburb: "Coomera", postcode: "4209", lat: -27.8486, lng: 153.3337 },
+  { suburb: "Oxenford", postcode: "4210", lat: -27.8906, lng: 153.3117 },
+  { suburb: "Helensvale", postcode: "4212", lat: -27.9150, lng: 153.3337 },
+  { suburb: "Hope Island", postcode: "4212", lat: -27.8696, lng: 153.3548 },
+  
+  // Sunshine Coast
+  { suburb: "Caloundra", postcode: "4551", lat: -26.8038, lng: 153.1218 },
+  { suburb: "Mooloolaba", postcode: "4557", lat: -26.6814, lng: 153.1189 },
+  { suburb: "Maroochydore", postcode: "4558", lat: -26.6552, lng: 153.0927 },
+  { suburb: "Buderim", postcode: "4556", lat: -26.6863, lng: 153.0563 },
+  { suburb: "Nambour", postcode: "4560", lat: -26.6271, lng: 152.9592 },
+  { suburb: "Coolum Beach", postcode: "4573", lat: -26.5278, lng: 153.0916 },
+  { suburb: "Noosa Heads", postcode: "4567", lat: -26.3942, lng: 153.0919 },
+  { suburb: "Noosaville", postcode: "4566", lat: -26.4042, lng: 153.0619 },
+  { suburb: "Tewantin", postcode: "4565", lat: -26.3892, lng: 153.0369 },
+  { suburb: "Maleny", postcode: "4552", lat: -26.7585, lng: 152.8515 },
+  
+  // Townsville
+  { suburb: "Townsville City", postcode: "4810", lat: -19.2590, lng: 146.8169 },
+  { suburb: "Aitkenvale", postcode: "4814", lat: -19.2990, lng: 146.7688 },
+  { suburb: "Douglas", postcode: "4814", lat: -19.3276, lng: 146.7474 },
+  { suburb: "Kirwan", postcode: "4817", lat: -19.3104, lng: 146.7260 },
+  { suburb: "Magnetic Island", postcode: "4819", lat: -19.1564, lng: 146.8491 },
+  
+  // Cairns
+  { suburb: "Cairns City", postcode: "4870", lat: -16.9186, lng: 145.7781 },
+  { suburb: "Cairns North", postcode: "4870", lat: -16.9020, lng: 145.7727 },
+  { suburb: "Edge Hill", postcode: "4870", lat: -16.9020, lng: 145.7404 },
+  { suburb: "Trinity Beach", postcode: "4879", lat: -16.7810, lng: 145.6973 },
+  { suburb: "Palm Cove", postcode: "4879", lat: -16.7366, lng: 145.6649 },
+  { suburb: "Port Douglas", postcode: "4877", lat: -16.4839, lng: 145.4642 },
+  { suburb: "Mossman", postcode: "4873", lat: -16.4687, lng: 145.3740 },
+  
+  // Toowoomba
+  { suburb: "Toowoomba City", postcode: "4350", lat: -27.5598, lng: 151.9507 },
+  { suburb: "South Toowoomba", postcode: "4350", lat: -27.5764, lng: 151.9507 },
+  { suburb: "East Toowoomba", postcode: "4350", lat: -27.5598, lng: 151.9729 },
+  { suburb: "North Toowoomba", postcode: "4350", lat: -27.5432, lng: 151.9618 },
+  { suburb: "West Toowoomba", postcode: "4350", lat: -27.5598, lng: 151.9285 },
+  { suburb: "Highfields", postcode: "4352", lat: -27.4655, lng: 151.9507 },
+  { suburb: "Oakey", postcode: "4401", lat: -27.4323, lng: 151.7173 },
+  { suburb: "Dalby", postcode: "4405", lat: -27.1830, lng: 151.2610 },
+  { suburb: "Warwick", postcode: "4370", lat: -28.2157, lng: 152.0284 },
+  { suburb: "Stanthorpe", postcode: "4380", lat: -28.6579, lng: 151.9340 },
+  
+  // Rockhampton
+  { suburb: "Rockhampton City", postcode: "4700", lat: -23.3791, lng: 150.5100 },
+  { suburb: "North Rockhampton", postcode: "4701", lat: -23.3458, lng: 150.5050 },
+  { suburb: "West Rockhampton", postcode: "4700", lat: -23.3813, lng: 150.4850 },
+  { suburb: "Yeppoon", postcode: "4703", lat: -23.1332, lng: 150.7447 },
+  { suburb: "Emu Park", postcode: "4710", lat: -23.2586, lng: 150.8272 },
+  { suburb: "Mount Morgan", postcode: "4714", lat: -23.6454, lng: 150.3875 },
+  { suburb: "Gracemere", postcode: "4702", lat: -23.4412, lng: 150.4575 },
+  
+  // Mackay
+  { suburb: "Mackay", postcode: "4740", lat: -21.1425, lng: 149.1868 },
+  { suburb: "North Mackay", postcode: "4740", lat: -21.1203, lng: 149.1793 },
+  { suburb: "South Mackay", postcode: "4740", lat: -21.1646, lng: 149.1793 },
+  { suburb: "Slade Point", postcode: "4740", lat: -21.0869, lng: 149.2168 },
+  { suburb: "Bucasia", postcode: "4750", lat: -21.0313, lng: 149.1568 },
+  { suburb: "Walkerston", postcode: "4751", lat: -21.1646, lng: 149.0593 },
+  { suburb: "Sarina", postcode: "4737", lat: -21.4201, lng: 149.2168 },
+  { suburb: "Proserpine", postcode: "4800", lat: -20.4014, lng: 148.5829 },
+  { suburb: "Airlie Beach", postcode: "4802", lat: -20.2682, lng: 148.7157 },
+  { suburb: "Cannonvale", postcode: "4802", lat: -20.2751, lng: 148.6970 },
+  
+  // Bundaberg
+  { suburb: "Bundaberg", postcode: "4670", lat: -24.8661, lng: 152.3489 },
+  { suburb: "Bundaberg East", postcode: "4670", lat: -24.8661, lng: 152.3733 },
+  { suburb: "Bundaberg North", postcode: "4670", lat: -24.8439, lng: 152.3489 },
+  { suburb: "Bundaberg South", postcode: "4670", lat: -24.8883, lng: 152.3489 },
+  { suburb: "Bundaberg West", postcode: "4670", lat: -24.8661, lng: 152.3245 },
+  { suburb: "Bargara", postcode: "4670", lat: -24.8217, lng: 152.4600 },
+  { suburb: "Burnett Heads", postcode: "4670", lat: -24.7583, lng: 152.4189 },
+  { suburb: "Coral Cove", postcode: "4670", lat: -24.8328, lng: 152.4733 },
+  
+  // Hervey Bay
+  { suburb: "Hervey Bay", postcode: "4655", lat: -25.2917, lng: 152.8517 },
+  { suburb: "Urangan", postcode: "4655", lat: -25.2917, lng: 152.9150 },
+  { suburb: "Torquay", postcode: "4655", lat: -25.2695, lng: 152.8761 },
+  { suburb: "Pialba", postcode: "4655", lat: -25.2917, lng: 152.8372 },
+  { suburb: "Scarness", postcode: "4655", lat: -25.2806, lng: 152.8517 },
+  { suburb: "Point Vernon", postcode: "4655", lat: -25.2583, lng: 152.8517 },
+  { suburb: "Kawungan", postcode: "4655", lat: -25.3139, lng: 152.8128 },
+  
+  // Gladstone
+  { suburb: "Gladstone", postcode: "4680", lat: -23.8439, lng: 151.2561 },
+  { suburb: "South Gladstone", postcode: "4680", lat: -23.8661, lng: 151.2561 },
+  { suburb: "West Gladstone", postcode: "4680", lat: -23.8439, lng: 151.2317 },
+  { suburb: "East Gladstone", postcode: "4680", lat: -23.8439, lng: 151.2806 },
+  { suburb: "New Auckland", postcode: "4680", lat: -23.8217, lng: 151.2561 },
+  { suburb: "Tannum Sands", postcode: "4680", lat: -23.9483, lng: 151.3639 },
+  { suburb: "Boyne Island", postcode: "4680", lat: -23.9594, lng: 151.3478 },
+  { suburb: "Agnes Water", postcode: "4677", lat: -24.2189, lng: 151.9000 },
+  { suburb: "Seventeen Seventy", postcode: "4677", lat: -24.1928, lng: 151.8917 },
+  
+  // Maryborough
+  { suburb: "Maryborough", postcode: "4650", lat: -25.5439, lng: 152.7028 },
+  { suburb: "Maryborough West", postcode: "4650", lat: -25.5439, lng: 152.6783 },
+  { suburb: "Tinana", postcode: "4650", lat: -25.5661, lng: 152.7272 },
+  { suburb: "Granville", postcode: "4650", lat: -25.5328, lng: 152.7150 },
+  { suburb: "Dundowran", postcode: "4655", lat: -25.4217, lng: 152.7906 },
+  
+  // Mount Isa
+  { suburb: "Mount Isa", postcode: "4825", lat: -20.7256, lng: 139.4927 },
+  
+  // Gympie
+  { suburb: "Gympie", postcode: "4570", lat: -26.1906, lng: 152.6650 },
+  { suburb: "Gympie South", postcode: "4570", lat: -26.2128, lng: 152.6650 },
+  { suburb: "Cooloola Cove", postcode: "4580", lat: -25.9328, lng: 153.0428 },
+  { suburb: "Rainbow Beach", postcode: "4581", lat: -25.9083, lng: 153.0917 },
+  
+  // Charters Towers
+  { suburb: "Charters Towers", postcode: "4820", lat: -20.0706, lng: 146.2611 },
+  
+  // Longreach
+  { suburb: "Longreach", postcode: "4730", lat: -23.4406, lng: 144.2544 },
+  
+  // Roma
+  { suburb: "Roma", postcode: "4455", lat: -26.5706, lng: 148.7856 },
+  
+  // Charleville
+  { suburb: "Charleville", postcode: "4470", lat: -26.4039, lng: 146.2611 },
+  
+  // Emerald
+  { suburb: "Emerald", postcode: "4720", lat: -23.5281, lng: 148.1597 },
+  
+  // Chinchilla
+  { suburb: "Chinchilla", postcode: "4413", lat: -26.7439, lng: 150.6289 },
+  
+  // Kingaroy
+  { suburb: "Kingaroy", postcode: "4610", lat: -26.5406, lng: 151.8361 },
+  
+  // Biloela
+  { suburb: "Biloela", postcode: "4715", lat: -24.4089, lng: 150.5100 },
+  
+  // Theodore
+  { suburb: "Theodore", postcode: "4719", lat: -25.0472, lng: 150.0933 },
+  
+  // Moura
+  { suburb: "Moura", postcode: "4718", lat: -24.5889, lng: 149.9783 },
+  
+  // Blackwater
+  { suburb: "Blackwater", postcode: "4717", lat: -23.5839, lng: 148.8828 },
+  
+  // Moranbah
+  { suburb: "Moranbah", postcode: "4744", lat: -22.0006, lng: 148.0456 }
+];
+
+// Dispatch event when suburbs are loaded
+if (typeof window !== 'undefined') {
+  window.dispatchEvent(new Event('suburbs-loaded'));
+}
